c963b749eac7f35a79c3e85bfcf9ae42e1e38f6b
andreeacirciu <andreeacirciu@gmail.com>
2020-06-22 18:57:30 +0300

    added logger in product service

--- src/main/java/com/example/onlineshop/service/ProductService.java
+++ src/main/java/com/example/onlineshop/service/ProductService.java
@@ -3,11 +3,15 @@ package com.example.onlineshop.service;
 import com.example.onlineshop.domain.Product;
 import com.example.onlineshop.persistence.ProductRepository;
 import com.example.onlineshop.transfer.SaveProductRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service //spring bean - obiectele carora li s-a cedat controlul si su ajuns in Io Container
 public class ProductService {
+
+    private static final Logger LOOGER = LoggerFactory.getLogger(ProductService.class);
 
     //IoC (Inversion of Control)
     private final ProductRepository productRepository; //nu am facut instantiere aici, am inversat controlul
@@ -20,7 +24,8 @@ public class ProductService {
 
     public Product createProduct(SaveProductRequest request) {
         // todo: replace with logger
-        System.out.println("Creating product: " + request);
+
+        LOOGER.info("Creating product {}", request);
 
         Product product = new Product();
         product.setName(request.getName());
